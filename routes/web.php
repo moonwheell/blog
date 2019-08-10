97ab58d5ec0180de82f58cd29f0038ee5b1bafa0
Your_Name <dimapetrenko.in@gmail.com>
2019-08-10 23:36:13 +0300

    lessons 12-13 admin category controller

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-//Route::resource('rest', 'RestTestController')->names('restTest');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Админка блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function() {
+    //BlogCategory
+    $methods = ['index','edit','update','create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
