4d0fe32b8d8b6ef9d10bb8e9580f8ba2c9438d7c
Your_Name <dimapetrenko.in@gmail.com>
2019-08-10 23:09:16 +0300

    lesson 11 base auth files

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -15,7 +15,6 @@ class PostController extends BaseController
     public function index()
     {
         $items = BlogPost::all();
-        dd($items->first());
         return view('blog.posts.index', compact('items'));
     }
 
