a4b0d10810893f5dd1238212175f9b0de7981fd7
Your_Name <dimapetrenko.in@gmail.com>
2019-08-13 20:47:44 +0300

    lesson 19 update method

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -14,6 +14,19 @@
                             <button class="close" type="button" data-dismiss="alert"
                                     aria-label="Close"><span aria-label="true">x</span>
                             </button>
+                            {{ $errors->first() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @if($errors->any())
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <div class="alert alert-danger" role="alert">
+                            <button class="close" type="button" data-dismiss="alert"
+                                    aria-label="Close"><span aria-label="true">x</span>
+                            </button>
+                            {{ $errors->first() }}
                         </div>
                     </div>
                 </div>
